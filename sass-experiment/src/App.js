636ac168a58f92a107ddb1f484397fbf4d637859
LucasbarcellosF <barcelos.lucas@gmail.com>
2020-11-23 01:35:03 -0300

    small fixes

--- sass-experiment/src/App.js
+++ sass-experiment/src/App.js
@@ -3,11 +3,12 @@ import Github from './img/Github.svg'
 import Linkedin from './img/Linkedin.svg'
 
 
-import './styles/style.scss'
+import './styles/style.css'
 
 function App() {
   return (
     <div>
+
       <header class="navbar">
         <nav>
           <li><a href="/about">About</a></li>
@@ -24,28 +25,22 @@ function App() {
       </section>
 
       <section class="second_section">
-
-        <h2 class="grid_label">These are some of my projects</h2>
-
+        <h2>These are some of my projects</h2>
         <div class="card_grid">
           <Card image="https://github.com/LucasbarcellosF/NLW/raw/master/example/1.png" label="Ecoleta" description="Next Level Week - Ecoleta" />
           <Card image="https://raw.githubusercontent.com/LucasbarcellosF/alertrack/master/public/aletrack.png" label="Aletrack" description="SPA Challenge - Alertrack" />
         </div>
-        
       </section>
 
       <section class="third_section">
-
-        <div class="third_section_label">
+        <div>
           <h3>Let's work together...</h3>
           <p>How do you take your coffee?</p>
         </div>
-
         <div class="section_list">
           <li><img src={Github} alt="github" /><a href="https://github.com/LucasbarcellosF">Github</a></li>
           <li><img src={Linkedin} alt="linkedin" /><a href="https://www.linkedin.com/in/lucasbarcellosf/">Linkedin</a></li>
         </div>
-
       </section>
 
     </div>
